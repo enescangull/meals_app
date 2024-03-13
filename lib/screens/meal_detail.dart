743eed90cc11cd4043a6cfaa1f6692e255446437
Enes Cangül <enescangull@gmail.com>
2024-03-13 14:16:02 +0300

    animations added

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -23,6 +23,7 @@ class MealDetailScreen extends ConsumerWidget {
                 final wasAdded = ref
                     .read(favoriteMealsProvider.notifier)
                     .toffleMealFavoriteStatus(meal);
+
                 ScaffoldMessenger.of(context).clearSnackBars();
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
@@ -31,18 +32,33 @@ class MealDetailScreen extends ConsumerWidget {
                   ),
                 );
               },
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border))
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 500),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+              ))
         ],
       ),
       body: ListView(
         children: [
           Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                height: 350,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 350,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(
                 height: 14,
